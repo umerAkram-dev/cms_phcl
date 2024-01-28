ff3756b2d14ad5b1e8fc9ed2f47cac3b6eb6d6c1
Umer Akram <umerakram.6932@gmail.com>
2024-01-28 18:46:38 +0500

    laravel version changed to 7.4

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,5 @@
 <?php
 
-use App\Http\Controllers\AboutUsController;
-use App\Http\Controllers\ContactController;
-use App\Http\Controllers\DonationController;
-use App\Http\Controllers\HomePageController;
-use App\Http\Controllers\ProgramHighlightsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,16 +8,19 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-// Route::get('/aboutus', function () {
-//     return view('aboutus');
-// });
-Route::get('aboutus', [AboutUsController::class, 'index'])->name('aboutus');
-Route::get('home', [HomePageController::class, 'index'])->name('home');
-Route::get('contact', [ContactController::class, 'index'])->name('contact');
-Route::get('donation', [DonationController::class, 'index'])->name('donation');
-Route::get('programhighlights', [ProgramHighlightsController::class, 'index'])->name('programhighlights');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::get('aboutus','AboutUsController@index')->name('aboutus');
+Route::get('/','HomePageController@index')->name('home');
+Route::get('contact','ContactController@index')->name('contact');
+Route::get('donation','DonationController@index')->name('donation');
+Route::get('programhighlights','ProgramHighlightsController@index')->name('programhighlights');
